5000274faf8a6a038dc0c78db9ba07c79ce580d7
jayesh <sjay05035@gmail.com>
2022-03-14 07:13:52 +0530

    add initial chat app files

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+// use rocket::tokio::select;
+// use rocket::{
+//     form::Form,
+//     response::stream::{Event, EventStream},
+//     serde::{Deserialize, Serialize},
+//     tokio::sync::broadcast::{channel, error::RecvError, Sender},
+//     Shutdown, State,
+// };
+
+use rocket::form::Form;
+use rocket::fs::{relative, FileServer};
+use rocket::response::stream::{Event, EventStream};
+use rocket::serde::{Deserialize, Serialize};
+use rocket::tokio::select;
+use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
+use rocket::{Shutdown, State};
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate=len(..20))]
+    pub username: String,
+    pub message: String,
+}
+
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send 'fails' if there are no active subscribers. That's okay.
+    let _res = queue.send(form.into_inner());
+}
+
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv()=> match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+            yield Event::json(&msg);
+        }
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("static")))
+}
